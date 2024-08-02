83d5701e4fc2677d77d573a150d345885c5426f1
Xavier RODRIGUEZ <xavier.rodriguezalvarez@gmail.com>
2024-08-02 11:48:48 +0200

    up

--- src/examples/test_cb.rs
+++ src/examples/test_cb.rs
@@ -2,6 +2,16 @@ use tokio::time::sleep;
 use tokio::time::Duration;
 
 use futures::future::BoxFuture;
+use futures::Future;
+
+type POK = fn(u8) -> BoxFuture<'static, Result<bool, ()>>;
+
+// fn wrap<F>(function: F) -> POK
+// where
+//     F: Future<Output = Result<bool, ()>> + Send + 'static,
+// {
+//     return |val: u8| -> BoxFuture<'static, Result<bool, ()>> { Box::pin(function) };
+// }
 
 fn test(val: u8) -> BoxFuture<'static, Result<bool, ()>> {
     Box::pin(async move {
@@ -10,13 +20,38 @@ fn test(val: u8) -> BoxFuture<'static, Result<bool, ()>> {
     })
 }
 
+struct Test {
+    b: Option<BoxFuture<'static, Result<bool, ()>>>,
+}
+
+impl Test {
+    fn set_b<F>(&mut self, function: F)
+    where
+        F: Future<Output = Result<bool, ()>> + Send + 'static,
+    {
+        self.b = Some(Box::pin(function));
+    }
+}
+
 #[tokio::main]
 async fn main() {
     println!("test cb");
 
-    let mut cbb: Option<fn(u8) -> BoxFuture<'static, Result<bool, ()>>> = None;
+    let mut cbb: Option<POK> = None;
 
     cbb = Some(test);
+
+    // let a = wrap(async move {
+    //     println!("neww!! !!!!");
+    //     Ok(true)
+    // });
+    // cbb = Some(a);
+
+    let mut abc = Test { b: None };
+    abc.set_b(async move {
+        println!("neww!! !!!!");
+        Ok(true)
+    });
 
     tokio::spawn(cbb.unwrap()(1));
 
