ebeab0d1f90546fe4d8a2ed8f71ab57cc173c4f6
XdoctorwhoZ <xavier.rodriguezalvarez@gmail.com>
2025-05-03 20:21:20 +0200

    si & string

--- tests/worlds/basics_world.rs
+++ tests/worlds/basics_world.rs
@@ -1,3 +1,5 @@
+mod si;
+mod string;
 mod reactor;
 mod boolean;
 
---
+++ tests/worlds/basics_world/si.rs
@@ -0,0 +1,12 @@
+use cucumber::{given, then, when};
+use panduza::reactor::ReactorOptions;
+
+use super::BasicsWorld;
+
+
+
+#[when(expr = "I set rw si to {float}")]
+fn i_set_rw_si_to(world: &mut BasicsWorld, f: f32) {
+    // Write code here that turns the phrase above into concrete actions
+}
+
