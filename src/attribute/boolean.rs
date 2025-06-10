6b161d698de2c40ec9c1466129c59908219aec4f
Xavier RODRIGUEZ <xavier.rodriguezalvarez@gmail.com>
2025-06-10 15:58:53 +0200

    up

--- src/attribute/boolean.rs
+++ src/attribute/boolean.rs
@@ -1,11 +1,11 @@
 // use crate::pubsub::Publisher;
+use super::{CallbackEntry, CallbackId};
 use crate::fbs::BooleanBuffer;
 use crate::AttributeMetadata;
+use std::collections::HashMap;
 use std::sync::Arc;
 use std::sync::Mutex;
 use zenoh::Session;
-use std::collections::HashMap;
-use super::{CallbackEntry, CallbackId};
 
 #[derive(Clone, Debug)]
 /// Object to manage the BooleanAttribute
@@ -16,7 +16,7 @@ pub struct BooleanAttribute {
     session: Session,
 
     /// Metadata for the attribute
-    /// 
+    ///
     metadata: AttributeMetadata,
 
     /// Callbacks storage
@@ -28,50 +28,59 @@ pub struct BooleanAttribute {
     next_callback_id: Arc<Mutex<CallbackId>>,
 
     /// Command topic
-    /// 
+    ///
     cmd_topic: String,
 
     /// Last received value
-    /// 
+    ///
     last_value: Arc<Mutex<Option<BooleanBuffer>>>,
 }
 
 impl BooleanAttribute {
     /// Create a new instance
     ///
-    pub async fn new(
-        session: Session,
-        metadata: AttributeMetadata,
-    ) -> Self {        
+    pub async fn new(session: Session, metadata: AttributeMetadata) -> Self {
         // Initialize callbacks storage
-        let callbacks = Arc::new(Mutex::new(HashMap::<CallbackId, CallbackEntry<BooleanBuffer>>::new()));
+        let callbacks = Arc::new(Mutex::new(HashMap::<
+            CallbackId,
+            CallbackEntry<BooleanBuffer>,
+        >::new()));
 
         // Trigger the callback mecanism on message reception
         let att_topic = format!("{}/att", &metadata.topic);
         let subscriber = session.declare_subscriber(att_topic).await.unwrap();
+        let last_value = Arc::new(Mutex::new(None));
         tokio::spawn({
             let callbacks = callbacks.clone();
+            let last_value = last_value.clone();
             async move {
-            while let Ok(sample) = subscriber.recv_async().await {
-                // Create BooleanBuffer from the received zbytes
-                let boolean_buffer = BooleanBuffer::from_zbytes(sample.payload().clone());
-                
-                // Trigger all callbacks
-                let callbacks_map = callbacks.lock().unwrap();
-                for (_id, callback_entry) in callbacks_map.iter() {
-                    // Check condition if present
-                    let should_trigger = if let Some(condition) = &callback_entry.condition {
-                        condition(&boolean_buffer)
-                    } else {
-                        true
-                    };
-                    
-                    if should_trigger {
-                        (callback_entry.callback)(&boolean_buffer);
+                while let Ok(sample) = subscriber.recv_async().await {
+                    // Create BooleanBuffer from the received zbytes
+                    let boolean_buffer = BooleanBuffer::from_zbytes(sample.payload().clone());
+
+                    // Met à jour la dernière valeur reçue
+                    {
+                        let mut last = last_value.lock().unwrap();
+                        *last = Some(boolean_buffer.clone());
+                    }
+
+                    // Trigger all callbacks
+                    let callbacks_map = callbacks.lock().unwrap();
+                    for (_id, callback_entry) in callbacks_map.iter() {
+                        // Check condition if present
+                        let should_trigger = if let Some(condition) = &callback_entry.condition {
+                            condition(&boolean_buffer)
+                        } else {
+                            true
+                        };
+
+                        if should_trigger {
+                            (callback_entry.callback)(&boolean_buffer);
+                        }
                     }
                 }
             }
-        }});
+        });
 
         // // Wait for the first message if mode is not readonly
         // if mode != AttributeMode::WriteOnly {
@@ -88,7 +97,6 @@ impl BooleanAttribute {
         //     pack.lock().unwrap().push(value);
         // }        //
 
-
         // Create the command topic
         let cmd_topic = format!("{}/cmd", &metadata.topic);
 
@@ -99,29 +107,22 @@ impl BooleanAttribute {
             callbacks: callbacks,
             next_callback_id: Arc::new(Mutex::new(0)),
             cmd_topic: cmd_topic,
-            last_value: Arc::new(Mutex::new(None)),
+            last_value: last_value,
         }
     }
 
-
-      /// Send command and do not wait for validation
+    /// Send command and do not wait for validation
     ///
     pub async fn shoot(&mut self, value: bool) {
         // Create a BooleanBuffer with the value
         let boolean_buffer = BooleanBuffer::from(value);
-        
+
         // Convert to bytes for sending
         let pyl = boolean_buffer.to_zbytes();
 
         // Send the command
         self.session.put(&self.cmd_topic, pyl).await.unwrap();
     }
-
-    // /// Notify when new data have been received
-    // ///
-    // pub fn update_notifier(&self) -> Arc<Notify> {
-    //     self.update_notifier.clone()
-    // }
 
     // ///
     // ///
@@ -148,29 +149,24 @@ impl BooleanAttribute {
     //     Ok(())
     // }
 
-    // ///
-    // ///
-    // pub fn get(&self) -> Option<bool> {
-    //     self.pack.lock().unwrap().last()
-    // }    // ///
-    // ///
-    // pub fn pop(&self) -> Option<bool> {
-    //     self.pack.lock().unwrap().pop()
-    // }
+    /// Get the last received value
+    ///
+    pub fn get(&self) -> Option<BooleanBuffer> {
+        self.last_value.lock().unwrap().clone()
+    }
 
-    
     /// Wait for a specific boolean value to be received
-    /// 
+    ///
     pub async fn wait_for_value(&self, value: bool) -> Result<(), String> {
         use tokio::sync::oneshot;
-        
+
         // Create a oneshot channel to signal when the value is received
         let (sender, receiver) = oneshot::channel();
         let sender_wrapped = Arc::new(Mutex::new(Some(sender)));
-        
+
         // Clone the sender for use in the callback closure
         let sender_for_callback = sender_wrapped.clone();
-        
+
         // Add a callback with condition to wait for the specific value
         let callback_id = self.add_callback_with_condition(
             move |_buffer| {
@@ -182,18 +178,19 @@ impl BooleanAttribute {
             move |buffer| {
                 // Condition: check if the received value matches the expected value
                 buffer.value() == value
-            }
+            },
         );
-        
+
         // Wait for the signal with a timeout
         let result = tokio::time::timeout(
             std::time::Duration::from_secs(10), // 10 seconds timeout
-            receiver
-        ).await;
-        
+            receiver,
+        )
+        .await;
+
         // Remove the callback
         self.remove_callback(callback_id);
-        
+
         match result {
             Ok(Ok(())) => Ok(()),
             Ok(Err(_)) => Err("Channel closed unexpectedly".to_string()),
@@ -209,15 +206,15 @@ impl BooleanAttribute {
     {
         let mut callbacks = self.callbacks.lock().unwrap();
         let mut next_id = self.next_callback_id.lock().unwrap();
-        
+
         let callback_id = *next_id;
         *next_id += 1;
-        
+
         let callback_entry = CallbackEntry {
             callback: Box::new(callback),
             condition: None,
         };
-        
+
         callbacks.insert(callback_id, callback_entry);
         callback_id
     }
@@ -231,15 +228,15 @@ impl BooleanAttribute {
     {
         let mut callbacks = self.callbacks.lock().unwrap();
         let mut next_id = self.next_callback_id.lock().unwrap();
-        
+
         let callback_id = *next_id;
         *next_id += 1;
-        
+
         let callback_entry = CallbackEntry {
             callback: Box::new(callback),
             condition: Some(Box::new(condition)),
         };
-        
+
         callbacks.insert(callback_id, callback_entry);
         callback_id
     }
