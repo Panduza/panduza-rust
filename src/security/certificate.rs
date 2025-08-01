2a67fc2a0ae8a1a42437ad485ad8e6ae70333272
Zisraw <bastien.huard@orange.fr>
2025-08-01 10:26:39 +0200

    clean certif lib

--- src/security/certificate.rs
+++ src/security/certificate.rs
@@ -1,15 +1,43 @@
+use super::utils::{load_key_from_pem, validity_period};
 use rcgen::{
     BasicConstraints, Certificate, CertificateParams, CertificateSigningRequest,
     CertificateSigningRequestParams, DnType, DnValue::PrintableString, ExtendedKeyUsagePurpose,
     IsCa, KeyPair, KeyUsagePurpose,
 };
-use std::fs;
-use time::{Duration, OffsetDateTime};
 
+const CA_VALIDITY_DAYS: i32 = 365000;
+
+/// Certificate parameters
+///
+/// # Fields
+///
+/// * `san` - Addresses and domains to connect to the router
+/// * `validity_days` - Validity days
+/// * `common_name` - Role of the user : writer.local, reader.local, logger.local, platform.local, admin.local
+///
+/// # Example
+///
+/// ```
+/// let cert_params = CertParams {
+///     san: vec!["localhost".into(), "127.0.0.1".into()],
+///     validity_days: 365,
+///     common_name: "writer.local".into(),
+/// };
+/// ```
+///
+pub struct CertParams {
+    pub san: Vec<String>,
+    pub validity_days: i32,
+    pub common_name: String,
+}
+
+/// ------ ROOT CA ------
+
+/// Set certificate authority parameters
 fn get_ca_params() -> CertificateParams {
     let mut params =
         CertificateParams::new(Vec::default()).expect("empty subject alt name can't produce error");
-    let (yesterday, tomorrow) = validity_period(365000);
+    let (yesterday, tomorrow) = validity_period(CA_VALIDITY_DAYS);
     params.is_ca = IsCa::Ca(BasicConstraints::Unconstrained);
     params.distinguished_name.push(
         DnType::CountryName,
@@ -30,78 +58,36 @@ fn get_ca_params() -> CertificateParams {
     return params;
 }
 
+/// Generate a root CA certificate and key
 pub fn generate_root_ca() -> (Certificate, KeyPair) {
     let params = get_ca_params();
 
     let key_pair = KeyPair::generate().unwrap();
-    let cert = params.clone().self_signed(&key_pair).unwrap();
-    (cert, key_pair)
+    let root_ca_cert = params.clone().self_signed(&key_pair).unwrap();
+    (root_ca_cert, key_pair)
 }
 
+/// Get a root CA certificate and key from a .pem key file
 pub fn get_root_ca(key_path: &str) -> (Certificate, KeyPair) {
     let key_pair = load_key_from_pem(key_path).unwrap();
     let params = get_ca_params();
-    let cert = params.clone().self_signed(&key_pair).unwrap();
-    (cert, key_pair)
+    let root_ca_cert = params.clone().self_signed(&key_pair).unwrap();
+    (root_ca_cert, key_pair)
 }
 
-pub fn generate_root_ca_with_key(key: &KeyPair) -> Certificate {
-    let params = get_ca_params();
-    let cert = params.clone().self_signed(&key).unwrap();
-    cert
-}
+/// ------ SERVER CERTIFICATE ------
 
-pub fn generate_cert_server(ca: &Certificate, ca_key: &KeyPair) -> (Certificate, KeyPair) {
-    let names = vec!["localhost".into(), "127.0.0.1".into()];
-    let mut params = CertificateParams::new(names).unwrap();
-    let (yesterday, tomorrow) = validity_period(365000);
-    params
-        .distinguished_name
-        .push(DnType::CommonName, "Panduza");
-    params.use_authority_key_identifier_extension = true;
-    params.key_usages.push(KeyUsagePurpose::DigitalSignature);
-    params.key_usages.push(KeyUsagePurpose::KeyEncipherment);
-    params
-        .extended_key_usages
-        .push(ExtendedKeyUsagePurpose::ServerAuth);
-    params.not_before = yesterday;
-    params.not_after = tomorrow;
-
-    let key_pair = KeyPair::generate().unwrap();
-    let cert = params.signed_by(&key_pair, ca, ca_key).unwrap();
-
-    (cert, key_pair)
-}
-
-pub fn generate_cert_client(ca: &Certificate, ca_key: &KeyPair) -> (Certificate, KeyPair) {
-    let names = vec!["localhost".into(), "127.0.0.1".into()];
-    let mut params = CertificateParams::new(names).unwrap();
-    let (yesterday, tomorrow) = validity_period(365000);
-    params
-        .distinguished_name
-        .push(DnType::CommonName, "Panduza");
-    params.use_authority_key_identifier_extension = true;
-    params.key_usages.push(KeyUsagePurpose::DigitalSignature);
-    params
-        .extended_key_usages
-        .push(ExtendedKeyUsagePurpose::ClientAuth);
-    params.not_before = yesterday;
-    params.not_after = tomorrow;
-
-    let key_pair = KeyPair::generate().unwrap();
-    let cert = params.signed_by(&key_pair, ca, ca_key).unwrap();
-    (cert, key_pair)
-}
-
+/// Generate a server certificate and key (for router) in the chain of trust
 pub fn generate_cert_server_with_san(
     ca: &Certificate,
     ca_key: &KeyPair,
-    san: &str,
+    cert_params: CertParams,
 ) -> (Certificate, KeyPair) {
-    let names = vec!["localhost".into(), "127.0.0.1".into(), san.into()];
-    let mut params = CertificateParams::new(names).unwrap();
-    let (yesterday, tomorrow) = validity_period(365000);
-    params.distinguished_name.push(DnType::CommonName, san);
+    let mut params = CertificateParams::new(cert_params.san).unwrap();
+    let (yesterday, tomorrow) = validity_period(cert_params.validity_days);
+    params
+        .distinguished_name
+        .push(DnType::CommonName, cert_params.common_name);
     params.use_authority_key_identifier_extension = true;
     params.key_usages.push(KeyUsagePurpose::DigitalSignature);
     params.key_usages.push(KeyUsagePurpose::KeyEncipherment);
@@ -116,16 +102,30 @@ pub fn generate_cert_server_with_san(
     (cert, key_pair)
 }
 
+/// Generate a server certificate and key (for router) in the chain of trust from the .pem root key file
+pub fn generate_cert_server_from_pem_with_san(
+    ca_key_path: &str,
+    cert_params: CertParams,
+) -> (Certificate, KeyPair) {
+    let (ca, key_pair) = get_root_ca(ca_key_path);
+
+    let (cert, key) = generate_cert_server_with_san(&ca, &key_pair, cert_params);
+    return (cert, key);
+}
+
+/// ------ CLIENT CERTIFICATE ------
+
+/// Generate a client certificate and key in the chain of trust
 pub fn generate_cert_client_with_san(
     ca: &Certificate,
     ca_key: &KeyPair,
-    san: &str,
-    days: i32,
+    cert_params: CertParams,
 ) -> (Certificate, KeyPair) {
-    let names = vec!["localhost".into(), "127.0.0.1".into(), san.into()];
-    let mut params = CertificateParams::new(names).unwrap();
-    let (yesterday, tomorrow) = validity_period(days);
-    params.distinguished_name.push(DnType::CommonName, san);
+    let mut params = CertificateParams::new(cert_params.san).unwrap();
+    let (yesterday, tomorrow) = validity_period(cert_params.validity_days);
+    params
+        .distinguished_name
+        .push(DnType::CommonName, cert_params.common_name);
     params.use_authority_key_identifier_extension = true;
     params.key_usages.push(KeyUsagePurpose::DigitalSignature);
     params
@@ -139,60 +139,27 @@ pub fn generate_cert_client_with_san(
     (cert, key_pair)
 }
 
-pub fn generate_cert_server_from_pem(key_path: &str) -> (Certificate, KeyPair) {
-    let (ca, key_pair) = get_root_ca(key_path);
-
-    let (cert, key) = generate_cert_server(&ca, &key_pair);
-    return (cert, key);
-}
-
-pub fn generate_cert_client_from_pem(key_path: &str) -> (Certificate, KeyPair) {
-    let (ca, key_pair) = get_root_ca(key_path);
-
-    let (cert, key) = generate_cert_client(&ca, &key_pair);
-    return (cert, key);
-}
-
-pub fn generate_cert_server_from_pem_with_san(key_path: &str, san: &str) -> (Certificate, KeyPair) {
-    let (ca, key_pair) = get_root_ca(key_path);
-
-    let (cert, key) = generate_cert_server_with_san(&ca, &key_pair, san);
-    return (cert, key);
-}
-
+/// Generate a client certificate and key in the chain of trust from the .pem root key file
 pub fn generate_cert_client_from_pem_with_san(
-    key_path: &str,
-    san: &str,
-    days: i32,
+    ca_key_path: &str,
+    cert_params: CertParams,
 ) -> (Certificate, KeyPair) {
-    let (ca, key_pair) = get_root_ca(key_path);
+    let (ca, key_pair) = get_root_ca(ca_key_path);
 
-    let (cert, key) = generate_cert_client_with_san(&ca, &key_pair, san, days);
+    let (cert, key) = generate_cert_client_with_san(&ca, &key_pair, cert_params);
     return (cert, key);
 }
 
-pub fn load_key_from_pem(key_path: &str) -> Result<KeyPair, Box<dyn std::error::Error>> {
-    let ca_key_pem = fs::read_to_string(key_path)?;
-    let ca_key = KeyPair::from_pem(&ca_key_pem)?;
+/// ------ CERTIFICATE SIGNING REQUEST ------
 
-    Ok(ca_key)
-}
-
-pub fn generate_key() -> KeyPair {
-    KeyPair::generate().expect("Fail to generate key")
-}
-
-fn validity_period(days: i32) -> (OffsetDateTime, OffsetDateTime) {
-    let day = Duration::new(86400, 0);
-    let yesterday = OffsetDateTime::now_utc().checked_sub(day).unwrap();
-    let after = OffsetDateTime::now_utc().checked_add(day * days).unwrap();
-    (yesterday, after)
-}
-
-pub fn generate_csr_client(client_key: &KeyPair) -> CertificateSigningRequest {
-    let names = vec!["localhost".into(), "127.0.0.1".into()];
-    let mut params = CertificateParams::new(names).unwrap();
-    let (yesterday, tomorrow) = validity_period(365);
+/// Generate a client certificate signing request
+pub fn generate_csr_client(
+    client_key: &KeyPair,
+    san: Vec<String>,
+    validity_days: i32,
+) -> CertificateSigningRequest {
+    let mut params = CertificateParams::new(san).unwrap();
+    let (yesterday, tomorrow) = validity_period(validity_days);
     params
         .extended_key_usages
         .push(ExtendedKeyUsagePurpose::ClientAuth);
@@ -204,15 +171,17 @@ pub fn generate_csr_client(client_key: &KeyPair) -> CertificateSigningRequest {
     return csr;
 }
 
+/// Sign a client certificate signing request with a CA certificate and key
 pub fn sign_csr_with_ca(
     csr_pem: &str,
     ca_cert: &Certificate,
     ca_key: &KeyPair,
     cn: &str,
+    validity_days: i32,
 ) -> Result<Certificate, rcgen::Error> {
     let mut csr_params = CertificateSigningRequestParams::from_pem(csr_pem)?;
 
-    let (yesterday, tomorrow) = validity_period(30);
+    let (yesterday, tomorrow) = validity_period(validity_days);
 
     csr_params.params.not_before = yesterday;
     csr_params.params.not_after = tomorrow;
--- src/security/utils.rs
+++ src/security/utils.rs
@@ -1,6 +1,15 @@
+use rcgen::KeyPair;
 use std::fs;
 use std::path::PathBuf;
+use time::{Duration, OffsetDateTime};
 
+pub enum PanduzaFileType {
+    Key,
+    Certificate,
+    Csr,
+}
+
+/// Ensure the .panduza directory exist or create it with .panduza/keys and .panduza/certificate directories
 pub fn ensure_panduza_dirs() -> (PathBuf, PathBuf) {
     let home_dir = dirs::home_dir().expect("Unable to find user folder");
     let panduza_dir = home_dir.join(".panduza");
@@ -13,13 +22,12 @@ pub fn ensure_panduza_dirs() -> (PathBuf, PathBuf) {
     (keys_dir, cert_dir)
 }
 
-pub enum PanduzaFileType {
-    Key,
-    Certificate,
-    Csr,
-}
-
-pub fn write_panduza_file(file_type: PanduzaFileType, filename: &str, content: &str) -> std::io::Result<PathBuf> {
+/// Write a file in the panduza directories depending on the file type
+pub fn write_panduza_file(
+    file_type: PanduzaFileType,
+    filename: &str,
+    content: &str,
+) -> std::io::Result<PathBuf> {
     let (keys_dir, cert_dir) = ensure_panduza_dirs();
     let path = match file_type {
         PanduzaFileType::Key => keys_dir.join(filename),
@@ -28,8 +36,9 @@ pub fn write_panduza_file(file_type: PanduzaFileType, filename: &str, content: &
     };
     std::fs::write(path.clone(), content)?;
     Ok(path)
-} 
+}
 
+/// Get the path to the panduza directories depending on the file type
 pub fn get_panduza_dir(file_type: PanduzaFileType) -> PathBuf {
     let (keys_dir, cert_dir) = ensure_panduza_dirs();
     match file_type {
@@ -39,36 +48,21 @@ pub fn get_panduza_dir(file_type: PanduzaFileType) -> PathBuf {
     }
 }
 
+/// Check if a key exists in the panduza directories
 pub fn key_exists(filename: &str) -> bool {
     let (keys_dir, _) = ensure_panduza_dirs();
     let key_path = keys_dir.join(filename);
     key_path.exists()
 }
 
+/// Check if a certificate exists in the panduza directories
 pub fn certificate_exists(filename: &str) -> bool {
     let (_, cert_dir) = ensure_panduza_dirs();
     let cert_path = cert_dir.join(filename);
     cert_path.exists()
 }
 
-pub fn list_existing_keys() -> std::io::Result<Vec<String>> {
-    let (keys_dir, _) = ensure_panduza_dirs();
-    let mut keys = Vec::new();
-    
-    if keys_dir.exists() {
-        for entry in fs::read_dir(keys_dir)? {
-            let entry = entry?;
-            if entry.file_type()?.is_file() {
-                if let Some(filename) = entry.file_name().to_str() {
-                    keys.push(filename.to_string());
-                }
-            }
-        }
-    }
-    
-    Ok(keys)
-} 
-
+/// Get the default credentials paths of a user to create a Reactor
 pub fn get_default_certificate_paths() -> (PathBuf, PathBuf, PathBuf) {
     let (keys_dir, cert_dir) = ensure_panduza_dirs();
     let root_ca_path = cert_dir.join("root_ca_certificate.pem");
@@ -77,15 +71,23 @@ pub fn get_default_certificate_paths() -> (PathBuf, PathBuf, PathBuf) {
     (root_ca_path, client_cert_path, client_key_path)
 }
 
-pub fn get_certificate_or_key_path(filename: &str) -> PathBuf {
-    let (keys_dir, cert_dir) = ensure_panduza_dirs();
-    
-    if filename.ends_with("certificate.pem") {
-        cert_dir.join(filename)
-    } else if filename.ends_with("key.pem") {
-        keys_dir.join(filename)
-    } else {
-        let home_dir = dirs::home_dir().expect("Unable to find user folder");
-        home_dir.join(".panduza").join(filename)
-    }
+/// Generate a validity period
+pub fn validity_period(days: i32) -> (OffsetDateTime, OffsetDateTime) {
+    let day = Duration::new(86400, 0);
+    let yesterday = OffsetDateTime::now_utc().checked_sub(day).unwrap();
+    let after = OffsetDateTime::now_utc().checked_add(day * days).unwrap();
+    (yesterday, after)
+}
+
+/// Load a key rcgen struct KeyPair from a .pem file
+pub fn load_key_from_pem(key_path: &str) -> Result<KeyPair, Box<dyn std::error::Error>> {
+    let key_pem = fs::read_to_string(key_path)?;
+    let key = KeyPair::from_pem(&key_pem)?;
+
+    Ok(key)
+}
+
+/// Generate a key rcgen struct KeyPair
+pub fn generate_key() -> KeyPair {
+    KeyPair::generate().expect("Fail to generate key")
 }
