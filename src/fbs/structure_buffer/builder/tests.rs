7786798774157c12fc5aa32fd88f678daed6e63e
XdoctorwhoZ <xavier.rodriguezalvarez@gmail.com>
2025-09-12 15:55:24 +0200

    up

--- src/fbs/structure_buffer/builder/tests.rs
+++ src/fbs/structure_buffer/builder/tests.rs
@@ -1,17 +1,25 @@
+// cargo test fbs::structure_buffer::builder
+
 use super::StructureBufferBuilder;
 
 #[test]
 fn test_insert_node_simple() {
-    let mut root = StructureBufferBuilder::default()
-        .with_name("root".to_string());
-    
+    let mut root = StructureBufferBuilder::default().with_name("root".to_string());
+
     let child_node = StructureBufferBuilder::default()
         .with_name("child".to_string())
         .with_node("Attribute".to_string());
-    
+
+    let class_node = StructureBufferBuilder::default()
+        .with_name("tototot".to_string())
+        .with_node("Class".to_string());
+
     // Insert node at path ["child"]
-    root.insert_node(vec!["child".to_string()], child_node);
-    
+    root.insert_node(vec!["tototot".to_string()], class_node);
+    root.insert_node(vec!["tototot".to_string(), "child".to_string()], child_node);
+
+    println!("Root after insertions: {:?}", root);
+
     // Verify the child was inserted
     assert!(root.is_children_exists_with_name("child"));
 }
